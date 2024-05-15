e06a5599f7fe2d833becf9ae0b80cd3296549308
marnix <marnixlorist@hotmail.com>
2024-05-15 08:39:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = 'Ik heb niet gefaald';
+        $b = 'die niet werken.';
+        $c = '10000';
+        echo $a . ' ik heb alleen ' . $c . ' manieren gevonden ' . $b; 
+        echo "<br>";
+
+        $a = 'die geen fout';
+        $b = 'en';
+        $c = 'niet.';
+        echo ' Mensen ' . $a . $b . ' maken, werken ' . $c;
+        echo "<br>";
+
+        $a = 'ry';
+        $b = ', but';
+        $c = 'fail';
+        echo 'T' . $a . ' and ' . $c . $b . ' never ' . $c . ' to t' . $a;
+        echo "<br>";
+
+        $a = "\"omgaan";
+        $b = "met";
+        $c = "teleurstellingen\"";
+        echo 'De cursus ' . $a . ' ' . $b . ' ' . $c . ' kan vanavond helaas niet doorgaan.';
+    ?>
+</body>
+</html>
