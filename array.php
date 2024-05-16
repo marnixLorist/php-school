51e6a637eddff0775b5421702b92d53a634ee71f
marnix <marnixlorist@hotmail.com>
2024-05-16 09:40:27 +0200

    array

---
+++ array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     $bomen = array("img/nfjehfo.jpg", "img/enfkhqewo.jpg", "img/qwogoq2.jpg",);
+     foreacht($bomen as $boom){
+            echo "<img src=img/bomen/'" .$boom. "'>";
+     }
+    ?>
+</body>
+</html>
